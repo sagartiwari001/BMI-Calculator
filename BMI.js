7afc8554073309561f3736770999bc8beec2d4f3
Sagar Tiwari <94246306+sagartiwari001@users.noreply.github.com>
2023-02-24 21:39:09 +0530

    Add files via upload
    Body Mass Index (BMI) is simply a guideline to track weight issues. It is a measurement of body fat based on weight. BMI calculation for females and males would differ. BMI is the amount of fat content in the body as related to height and weight.

---
+++ BMI.js
@@ -0,0 +1,39 @@
+let button = document.getElementById('btn');
+
+button.addEventListener('click', () => {
+
+    const height = parseInt(document.getElementById('height').value);
+    const weight = parseInt(document.getElementById('weight').value);
+    const result = document.getElementById('output');
+    let height_status = false, weight_status = false;
+
+    if (height === '' || isNaN(height) || (height <= 0)) {
+        document.getElementById('height-error').innerHTML = 'Please provide a valid height';
+    } else {
+        document.getElementById('height-error').innerHTML = '';
+        height_status = true
+    }
+    if (weight == '' || isNaN(weight) || (weight <= 0)) {
+        document.getElementById('weight-error').innerHTML = 'Please provide a valid weight';
+    } else {
+        document.getElementById('weight-error').innerHTML = '';
+        weight_status = true
+    }
+    if (height_status && weight_status) {
+        const bmi = (weight / ((height * height) / 10000).toFixed(2));
+
+        if (bmi < 18.6) {
+            result.innerHTML = 'under weight : ' + bmi;
+
+        } else if (bmi >= 18.6 && bmi < 24.9) {
+            result.innerHTML = ' Normal : ' + bmi;
+        } else  {
+              result.innerHTML = 'Over Weight : ' + bmi;
+        } 
+    }
+    else
+    {
+        alert('Form Has Errors');
+        result.innerHTML ='';
+    }
+});
